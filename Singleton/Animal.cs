70123f2263d5fed44b5121bdffc98b7ae219098f
Jason Collier <JCollier@coronation.com>
2021-05-24 21:49:54 +0200

    Add project files.

---
+++ Singleton/Animal.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Singleton
+{
+
+    /// The 'AbstractFactory' abstract class
+    abstract class SpeciesFactory
+    {
+        public abstract Herbivore CreateOmnivore();
+        public abstract Carnivore CreateCarnivore();
+    }
+
+
+    /// The 'ConcreteFactory1' class
+
+    class DogFactory : SpeciesFactory
+
+    {
+        public override Herbivore CreateOmnivore()
+        {
+            return new Poodle();
+        }
+
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
+        }
+    }
+
+
+    /// The 'ConcreteFactory2' class
+
+    class CatFactory : SpeciesFactory
+
+    {
+        public override Herbivore CreateOmnivore()
+        {
+            return new Siamese();
+        }
+
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
+        }
+    }
+
+
+    /// The 'AbstractProductA' abstract class
+
+    abstract class Herbivore
+
+    {
+    }
+
+
+    /// The 'AbstractProductB' abstract class
+
+    abstract class Carnivore
+
+    {
+        public abstract void Eat(Herbivore h);
+    }
+
+
+    /// The 'ProductA1' class
+
+    class Poodle : Herbivore
+
+    {
+    }
+
+    /// The 'ProductB1' class
+
+    class Wolf : Carnivore
+
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Poodle
+
+            System.Console.WriteLine(this.GetType().Name +
+                                     " eats " + h.GetType().Name);
+        }
+    }
+
+    /// The 'ProductA2' class
+
+    class Siamese : Herbivore
+
+    {
+    }
+
+
+    /// The 'ProductB2' class
+
+    class Lion : Carnivore
+
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Siamese
+
+            System.Console.WriteLine(this.GetType().Name +
+                                     " eats " + h.GetType().Name);
+        }
+    }
+
+    /// The 'Client' class 
+
+    class AnimalWorld
+
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
+
+        // Constructor
+
+        public AnimalWorld(SpeciesFactory factory)
+        {
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateOmnivore();
+        }
+
+        public void RunFoodChain()
+        {
+            _carnivore.Eat(_herbivore);
+        }
+    }
+}
+
+//read all (Java and C#)
+//2. Builder pattern: build sounds / traits
+//3. Abstract Factory: validate (abstract)
---
+++ Singleton/Gadget.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    internal class Shop
+    {
+        // Builder uses a complex series of steps
+        public void Construct(GadgetBuilder gadgetBuilder)
+        {
+            gadgetBuilder.BuildScreen();
+            gadgetBuilder.BuildCasing();
+            gadgetBuilder.BuildMotherboard();
+        }
+    }
+
+
+    ///     The 'Builder' abstract class
+    internal abstract class GadgetBuilder
+    {
+        protected Device device;
+
+        // Gets device instance
+
+        public Device Device => device;
+
+        // Abstract build methods
+
+        public abstract void BuildScreen();
+        public abstract void BuildCasing();
+        public abstract void BuildMotherboard();
+    }
+
+    ///     The 'ConcreteBuilder1' class
+    internal class WatchBuilder : GadgetBuilder
+
+    {
+        public WatchBuilder()
+        {
+            device = new Device("Watch");
+        }
+
+        public override void BuildScreen()
+        {
+            device["screen"] = "218 x 218 pixels";
+        }
+
+        public override void BuildCasing()
+        {
+            device["casing"] = "Stainless steel";
+        }
+
+        public override void BuildMotherboard()
+        {
+            device["motherboard"] = "Small";
+        }
+    }
+
+
+    ///     The 'ConcreteBuilder2' class
+    internal class PhoneBuilder : GadgetBuilder
+
+    {
+        public PhoneBuilder()
+        {
+            device = new Device("Phone");
+        }
+
+        public override void BuildScreen()
+        {
+            device["screen"] = "1080 x 1920 pixels";
+        }
+
+        public override void BuildCasing()
+        {
+            device["casing"] = "Glass";
+        }
+
+        public override void BuildMotherboard()
+        {
+            device["motherboard"] = "Medium";
+        }
+    }
+
+    ///     The 'ConcreteBuilder3' class
+    internal class LaptopBuilder : GadgetBuilder
+
+    {
+        public LaptopBuilder()
+        {
+            device = new Device("Laptop");
+        }
+
+        public override void BuildScreen()
+        {
+            device["screen"] = "1920 x 1080 pixels";
+        }
+
+        public override void BuildCasing()
+        {
+            device["casing"] = "Plastic";
+        }
+
+        public override void BuildMotherboard()
+        {
+            device["motherboard"] = "Large";
+        }
+    }
+
+    ///     The 'Product' class
+    internal class Device
+
+    {
+        private readonly string _deviceType;
+
+        private readonly Dictionary<string, string> _parts =
+            new Dictionary<string, string>();
+
+        // Constructor
+
+        public Device(string deviceType)
+        {
+            _deviceType = deviceType;
+        }
+
+        // Indexer
+
+        public string this[string key]
+        {
+            get => _parts[key];
+            set => _parts[key] = value;
+        }
+
+        public void Show()
+        {
+            System.Console.WriteLine("Device Type: {0}", _deviceType);
+            System.Console.WriteLine(" Screen resolution : {0}", _parts["screen"]);
+            System.Console.WriteLine(" casing material : {0}", _parts["casing"]);
+            System.Console.WriteLine(" motherboard size: {0}", _parts["motherboard"]);
+            System.Console.WriteLine("\n");
+        }
+    }
+}
